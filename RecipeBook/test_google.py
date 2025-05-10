a120b22fd8e6f79906edb99cb37b7da59114a261
L4m4w <l4p41@yandex.com>
2025-05-10 12:45:00 +0300

    custom locator

--- RecipeBook/test_google.py
+++ RecipeBook/test_google.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
 
@@ -8,5 +11,21 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 driver = webdriver.Chrome()
 
 driver.get('https://google.com')
+
+assert driver.find_element(By.CSS_SELECTOR, '[name=q]').get_attribute('value') == ''
+
+class element_value_is_empty(object):
+    def __init__(self, selector):
+        self.selector = selector
+
+
+    def __call__(self, driver):
+        return driver.find_element(By.CSS_SELECTOR, self.selector).get_attribute('value') == ''
+
+wait = WebDriverWait(driver= driver, timeout=4)
+wait.until(element_value_is_empty('[name=q]'))
+
+driver.find_element(By.CSS_SELECTOR, '[name=q]').send_keys('google')
+driver.find_element(By.CSS_SELECTOR, '[name=q]').clear()
 # driver.
 # driver.get()
